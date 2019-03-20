b7b1446295e685ca303165e58335efbe68974d9e
soppya2497 <44817633+soppya2497@users.noreply.github.com>
2019-03-20 23:39:18 +0530

    Writing about the out

--- Hello_world.java
+++ Hello_world.java
@@ -2,6 +2,7 @@ class Demo{
   Static public void main(String args[])
   {
   //System class
+  //out is static reference variable of the Printstream class
   system.out.println("Hello World");
   }
   }
