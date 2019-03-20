17200dfc001cd8cbfe1a51bef5f2c4537bf34507
soppya2497 <44817633+soppya2497@users.noreply.github.com>
2019-03-20 23:42:13 +0530

    println method

--- Hello_world.java
+++ Hello_world.java
@@ -3,6 +3,7 @@ class Demo{
   {
   //System class
   //out is static reference variable of the Printstream class
+  //println is a method that'll display whatever written in the double quotes directly on output display
   system.out.println("Hello World");
   }
   }
