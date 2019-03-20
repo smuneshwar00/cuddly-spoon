475e0c9da61819c8f3a423d931408435189bf1d9
SwapnilM <smuneshwar00@gmail.com>
2019-03-21 00:44:03 +0530

    first commit after creating a java prjct and teaming up

--- Hello_world.java
+++ Hello_world.java
@@ -5,8 +5,8 @@ class Demo{
   //System class
   //out is static reference variable of the Printstream class
   //println is a method that'll display whatever written in the double quotes directly on output display
-  system.out.println("Hello World");
+  System.out.println("Hello World");
   
-  System.out.print("Hello github project in eclipse")
+  System.out.print("Hello github project in eclipse");
   }
   }
---
+++ Test on githuib/src/firstonGIT/Hello.java
@@ -0,0 +1,17 @@
+package firstonGIT;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//System class
+		  //out is static reference variable of the Printstream class
+		  //println is a method that'll display whatever written in the double quotes directly on output display
+		  System.out.println("Hello World");
+		  
+		  System.out.print("Hello github project in eclipse");
+		
+	}
+
+}
